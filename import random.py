6a4b1d25d599b77d733af714d8a04365f0d2704c
ignaciolc09 <ignaciolomelicortes@gmail.com>
2026-05-20 19:57:29 -0700

    Commit?? learning git

---
+++ import random.py
@@ -0,0 +1,28 @@
+import random
+
+
+jokes = ["Why don't scientists trust atoms? Because they make up everything.",
+         "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+         "Why can't a bike stand on its own? It's two-tired.",
+         "I'm reading a book about anti-gravity. It's impossible to put down.",
+         "Why did the scarecrow win an award? He was outstanding in his field.",
+         "I used to hate facial hair, but then it grew on me.",
+         "Why don't eggs tell jokes? They'd crack each other up.",
+         "I would tell a construction joke, but I'm still working on it.",
+         "What do you call a fake noodle? An impasta.",
+         "Did you hear about the guy who invented Lifesavers? He made a mint."
+         "What do you call cheese that isn't yours? Nacho cheese.",
+         "Why did the math book look sad? Because it had too many problems.",
+         "Why did the tomato turn red? Because it saw the salad dressing.",
+         "Why did the golfer bring two pairs of pants? In case he got a hole in one.",
+         "What do you call a bear with no teeth? A gummy bear.",
+         "Why don't skeletons fight each other? They don't have the guts."]
+
+for i in random.sample(jokes, len(jokes)):
+    input("Press Enter for a joke...")
+    print(i)
+print("That's all the jokes for now! Hope you enjoyed them!")
+
+##hi guysss
+print("hi guysss")
+
